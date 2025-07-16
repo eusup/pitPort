2ed567c3da4e39529d6d7baa3ef8b309e208c95b
eusup-chungchy <167936045+eusup@users.noreply.github.com>
2025-07-16 13:45:33 +0900

    Add files via upload

---
+++ resource/js/style.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(function() {
+    var page = $('#fullpage').fullpage({
+        navigation : true,
+        navigationPosition : 'left',
+        sectionsColor : ['pink', 'yellow', '#efefef'],
+    });
+})
+});
