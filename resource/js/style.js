48484ea0a59fbce168e89102983d07eace23b5da
eusup <yusup.lee@chungchy.com>
2025-07-16 15:29:35 +0900

    두번째섹션

--- resource/js/style.js
+++ resource/js/style.js
@@ -1,8 +1,27 @@
-$(document).ready(function() {
-    $(function() {
-    var page = $('#fullpage').fullpage({
-        // navigation : true,
-        navigationPosition : 'right',
+$(document).ready(function () {
+    $(function () {
+        var page = $('#fullpage').fullpage({
+            // navigation : true,
+            navigationPosition: 'right',
+        });
+    })
+
+    var sections = $('.section01, .section02, .section03');
+
+    sections.each(function () {
+        var $section = $(this);
+        var hadActive = $section.hasClass('active');
+
+        setInterval(function () {
+            var hasActive = $section.hasClass('active');
+
+            if (hadActive && !hasActive) {
+                $section.addClass('act');
+            } else if (!hadActive && hasActive) {
+                $section.removeClass('act');
+            }
+
+            hadActive = hasActive;
+        }, 100);
     });
-})
 });
